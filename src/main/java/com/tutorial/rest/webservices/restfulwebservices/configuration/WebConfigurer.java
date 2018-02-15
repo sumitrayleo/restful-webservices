24f8f773bc62189307524ccf842579ac15486360
Sumit Ray <sumit.ray@ehi.com>
2018-02-15 17:17:21 +0530

    Configuring Swagger2

--- src/main/java/com/tutorial/rest/webservices/restfulwebservices/configuration/WebConfigurer.java
+++ src/main/java/com/tutorial/rest/webservices/restfulwebservices/configuration/WebConfigurer.java
@@ -3,6 +3,7 @@ package com.tutorial.rest.webservices.restfulwebservices.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import com.tutorial.rest.webservices.restfulwebservices.interceptor.CustomHeaderInterceptor;
@@ -16,6 +17,12 @@ public class WebConfigurer extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(httpHeaderInterceptor);
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
